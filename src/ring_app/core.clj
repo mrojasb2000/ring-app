d92ad977fa6548ccd271c396cd81a2ca62167c6d
Mauricio Rojas <mrojasb2000@gmail.com>
2023-01-06 23:57:04 -0300

    feat: added format string and handler functions

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,6 +1,32 @@
-(ns ring-app.core)
+(ns ring-app.core
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.util.response :as response]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn formatString  [label key map]
+  (str  "<p><b>" label ": </b>"
+    (key map)
+    "</p>"))
+
+(defn handler [request-map]
+  (response/response
+    (str "<html><body> Request data: "
+        (formatString "server port" :server-port request-map)
+        (formatString "server name" :server-name request-map)
+        (formatString "remote address" :remote-addr request-map)
+        (formatString "query string" :query-string request-map)
+        (formatString "scheme" :scheme request-map)
+        (formatString "request method" :request-method  request-map)
+        (formatString "content type" :content-type  request-map)
+        (formatString "content-length" :content-length  request-map)
+        (formatString "character endcoding" :character-endcoding  request-map)
+        (formatString "headers" :headers  request-map)
+        (formatString "context" :context  request-map)
+        (formatString "uri" :uri  request-map)
+        (formatString "ssl-client-cert" :ssl-client-cert  request-map)
+        "</p></body></html>")))
+
+(defn -main []
+  (jetty/run-jetty
+   handler
+   {:port 3000
+    :join? false}))
