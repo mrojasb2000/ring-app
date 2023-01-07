0783fe11527ba92470b222773b5ac42db2c029e0
Mauricio Rojas <mrojasb2000@gmail.com>
2023-01-07 02:40:42 -0300

    feat: added basic no-cache  middleware wrapper

--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -25,8 +25,14 @@
         (formatString "ssl-client-cert" :ssl-client-cert  request-map)
         "</p></body></html>")))
 
+(defn wrap-nocache [handler]
+  (fn [request]
+    (-> request
+        handler
+        (assoc-in [:headers "Pragma"] "no-cache"))))
+
 (defn -main []
   (jetty/run-jetty
-   handler
+   (-> handler wrap-nocache)
    {:port 3000
     :join? false}))
