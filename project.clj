b6427e51d5b4071a835ed63576315b4b9df099a2
Mauricio Rojas <mrojasb2000@gmail.com>
2023-01-10 20:39:23 -0300

    chore: change Muuntaja library version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.11.1"]
-                 [metosin/muuntaja "0.6.7"]
+                 [metosin/muuntaja "0.6.8"]
                  [metosin/ring-http-response "0.9.1"]
                  [ring "1.8.2"]]
   :repl-options {:init-ns ring-app.core}
